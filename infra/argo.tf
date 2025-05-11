533519a11446579759e51d37579709445ad061ef
LAREDO LANCER <lancerajayi@yahoo.com>
2025-05-11 17:24:18 +0100

    latset update to terraform script

--- infra/argo.tf
+++ infra/argo.tf
@@ -1,56 +1,45 @@
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
 resource "kubernetes_namespace" "argocd" {
   metadata {
     name = "argocd"
   }
 }
 
-resource "kubernetes_manifest" "argocd_install" {
-  manifest = [
-    {
-      apiVersion = "v1"
-      kind       = "Namespace"
-      metadata = {
-        name = "argocd"
+resource "kubernetes_deployment" "argocd_server" {
+  metadata {
+    name      = "argocd-server"
+    namespace = kubernetes_namespace.argocd.metadata[0].name
+  }
+
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "argocd-server"
       }
-    },
-    {
-      apiVersion = "apps/v1"
-      kind       = "Deployment"
-      metadata = {
-        name      = "argocd-server"
-        namespace = "argocd"
-      }
-      spec = {
-        replicas = 1
-        selector = {
-          matchLabels = {
-            app = "argocd-server"
-          }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "argocd-server"
         }
-        template = {
-          metadata = {
-            labels = {
-              app = "argocd-server"
-            }
-          }
-          spec = {
-            containers = [
-              {
-                name  = "argocd-server"
-                image = "argoproj/argocd:v2.9.0"
-                ports = [
-                  {
-                    containerPort = 443
-                  }
-                ]
-              }
-            ]
+      }
+      spec {
+        container {
+          name  = "argocd-server"
+          image = "argoproj/argocd:v2.9.0"
+          port {
+            container_port = 443
           }
         }
       }
     }
-  ]
+  }
 }
+
 resource "kubernetes_service" "argocd_server" {
   metadata {
     name      = "argocd-server"
@@ -62,9 +51,36 @@ resource "kubernetes_service" "argocd_server" {
       app = "argocd-server"
     }
 
+    type = "NodePort"  # Updated for local access
+
     port {
       port        = 443
-      target_port = 443
+      target_port = kubernetes_deployment.argocd_server.spec[0].template[0].spec[0].container[0].port[0].container_port
+      node_port   = 30443  # Accessible via http://localhost:30443
+    }
+  }
+}
+
+resource "kubernetes_ingress" "argocd_server" {
+  metadata {
+    name      = "argocd-server"
+    namespace = kubernetes_namespace.argocd.metadata[0].name
+  }
+
+  spec {
+    rule {
+      host = "argocd.local"  # Updated for local access
+
+      http {
+        path {
+          path      = "/"  
+
+          backend {
+            service_name = kubernetes_service.argocd_server.metadata[0].name
+            service_port = kubernetes_service.argocd_server.spec[0].port[0].port
+          }
+        }
+      }
     }
   }
 }
--- infra/main.tf
+++ infra/main.tf
@@ -2,10 +2,22 @@ provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
+resource "kubernetes_namespace" "default_ns" {
+  metadata {
+    name = "default"
+  }
+}
+
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd"
+  }
+}
+
 resource "kubernetes_deployment" "myapp" {
   metadata {
-    name = "myapp"
-    namespace = "default"
+    name      = "myapp"
+    namespace = kubernetes_namespace.default_ns.metadata[0].name
   }
   spec {
     replicas = 2
@@ -23,7 +35,84 @@ resource "kubernetes_deployment" "myapp" {
       spec {
         container {
           name  = "app"
-          image = "nginx"
+          image = "ghs-nginx-app42"
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "myapp_service" {
+  metadata {
+    name      = "myapp-service"
+    namespace = kubernetes_namespace.default_ns.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = "myapp"
+    }
+
+    type = "NodePort"
+
+    port {
+      port        = 80
+      target_port = kubernetes_deployment.myapp.spec[0].template[0].spec[0].container[0].port[0].container_port
+      node_port   = 30080  # Accessible via http://localhost:30080
+    }
+  }
+}
+
+resource "kubernetes_ingress" "myapp_ingress" {
+  metadata {
+    name      = "myapp-ingress"
+    namespace = kubernetes_namespace.default_ns.metadata[0].name
+  }
+
+  spec {
+    rule {
+      host = "myapp.local"
+
+      http {
+        path {
+          path      = "/"
+          # path_type attribute removed as it is not supported
+          backend {
+            service_name = kubernetes_service.myapp_service.metadata[0].name
+            service_port = kubernetes_service.myapp_service.spec[0].port[0].port
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_manifest" "argocd_application_myapp" {
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = "myapp"
+      namespace = kubernetes_namespace.argocd.metadata[0].name
+    }
+    spec = {
+      project = "default"
+      source = {
+        repoURL = "https://github.com/your-repo/myapp.git"
+        path    = "infra"
+        targetRevision = "main"
+      }
+      destination = {
+        namespace = kubernetes_namespace.default_ns.metadata[0].name
+        server    = "https://kubernetes.default.svc"
+      }
+      syncPolicy = {
+        automated = {
+          prune    = true
+          selfHeal = true
         }
       }
     }
