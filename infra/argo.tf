c616d41e4b2c1ff68dffd69b24ddc11a3c8c243f
LAREDO LANCER <lancerajayi@yahoo.com>
2025-05-12 13:46:04 +0100

    docker.io/argoproj/argocd:latest

--- infra/argo.tf
+++ infra/argo.tf
@@ -31,7 +31,7 @@ resource "kubernetes_deployment" "argocd_server" {
       spec {
         container {
           name  = "argocd-server"
-          image = "argoproj/argocd:latest" # Corrected image reference
+          image = "docker.io/argoproj/argocd:latest" # Corrected image reference
 
           resources {
             requests = {
